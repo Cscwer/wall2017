a2916ce921ac60b03b50b4a8a39281fdd0ffda64
eczn <1975845422@qq.com>
2017-09-29 00:17:13 +0800

    把 api 挂在 /api 上

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', index);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
