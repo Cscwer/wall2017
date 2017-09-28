fd88680f145310c308b4a9e15fc21a51eb24dae4
eczn <1975845422@qq.com>
2017-09-29 00:22:32 +0800

    从微信获得用户授权进而取得用户资料并存储在 cookie 中

--- routes/entry.js
+++ routes/entry.js
@@ -2,7 +2,17 @@
 const express = require('express')
     , router = express.Router()
     , qs = require('querystring')
-    , { redirect_param } = require('../config')
+    , redirect_param = {
+		appid: 'wxca79087c2e5cf375', 
+		redirect_uri: 'http://gw.chenpt.cc/api/entry/code', 
+		response_type: 'code', 
+		scope: 'snsapi_userinfo',
+		state: '0' 
+	}
+	, vwx = require('../utils/vwx')
+	, { userModel } = require('../utils/db')
+	, auth = require('../utils/auth')
+	, R = require('../utils/redis')
 
 
 let redirectTo = 
@@ -10,8 +20,69 @@ let redirectTo =
 	qs.stringify(redirect_param) + 
 	'#wechat_redirect'; 
 
+
 router.get('/', function(req, res, next) {
-	res.redirect(redirectTo);
+	if (req.cookies['user-token']){
+		// 说明应该登录过 
+		res.redirect('/');
+	} else {
+		res.redirect(redirectTo);
+	}
+});
+
+router.get('/code', function(req, res){
+	// URL Such As: 
+	// https://gw.chenpt.cc/?code=0814YGn215ypHM1afTn21zSHn214YGne&state=0 
+	let { code } = req.query; 
+	let openid ;
+
+	vwx.code2user(code).then(user => {
+		// User openid
+		openid = user.openid; 
+
+		let data = new userModel(user); 
+
+		return data.save()
+	}).then(user => {
+		// 用户注册成功 
+		// 设置 cookie 表示用户已经注册 
+		let token = auth.en(user); 
+
+		res.cookie('user-token', token, {
+			expires: new Date('2017-11-11')
+		});
+		res.redirect('/'); 
+	}, err => {
+		// 可能的错误： 
+		// 1. 数据库重复项 err.code === 11000 
+		// 2. 数据校验失败 
+		// 3. 数据库内部错误 
+		if (err.code === 11000){
+			return userModel.findOne({
+				openid: openid
+			}); 
+		} else {
+			console.log(err);
+
+			return Promise.reject(err); 
+		}
+	}).then(user => {
+		console.log(`用户重复`)
+
+		let token = auth.en(user); 
+
+		res.cookie('user-token', token, {
+			expires: new Date('2017-11-11')
+		})
+
+		res.redirect('/'); 
+	}, err => {
+		console.log(err); 
+		res.json({
+			code: 500, 
+			err: err
+		});
+	})
 });
 
 module.exports = router;
