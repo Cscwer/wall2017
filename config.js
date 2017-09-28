aa0e6cacb1e0da7c34877d99a480f690fb38918b
eczn <1975845422@qq.com>
2017-09-29 00:17:46 +0800

    正确的微信 config

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 // config.js
 module.exports = {
 	redirect_param: {
-		appid: `APPID`, 
-		redirect_uri: 'asdasd', 
+		appid: 'wxca79087c2e5cf375', 
+		redirect_uri: 'https://gw.chenpt.cc', 
 		response_type: 'code', 
-		scope: 'SCOPE',
-		state: 'STATE' 
-	}
+		scope: 'snsapi_userinfo',
+		state: '0' 
+	},
+	AppID: 'wxca79087c2e5cf375', 
+	AppSecret: '0dfb3847153ade322b36d42323e91dba'
 }
