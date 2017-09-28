9fc501dd8723917b9e8c1211642136f8ddfedeb6
eczn <1975845422@qq.com>
2017-09-29 00:21:25 +0800

    为 auth 添加 redis 缓存

--- utils/auth/index.js
+++ utils/auth/index.js
@@ -1,28 +1,47 @@
 // index.js
 const jwt = require('jsonwebtoken')
-    , SECRET = `This_Is_Secret_Key`; 
+    , SECRET = `This_Is_Secret_Key`
+    , R = require('../redis')
 
 let auth = {}; 
 
-auth.get = function(data = {}){
-	return jwt.sign({
+auth.en = function(data = {}){
+	let token = jwt.sign({
 		// 一个钟 
 		exp: Math.floor(Date.now() / 1000) + 99000000, 
 		data: data
 	}, SECRET);
+
+	// 缓存 
+	// console.log(data); 
+	R.set(token, JSON.stringify(data));
+
+	return token; 
 }
 
-auth.check = function(token){
+auth.de = function(token){
 	let data;
 
-	try {
-		data = jwt.verify(token, SECRET);
-		return data; 
-	} catch (err) {
-		return false; 
-	}
+	return R.get(token).then(val => {
+		if (val){
+			// 缓存命中 
+			return JSON.parse(val); 
+		} else {
+			// 否则尝试解开 
+			try {
+				opt = jwt.verify(token, SECRET);
 
-	return data; 
+				// 重置缓存 
+				R.set(token, JSON.stringify(opt.data));
+
+				return opt.data; 
+			} catch (err) {
+				// 如果出错 证明该 Token 不合法 
+				console.log(err)
+				return Promise.reject(false); 
+			}
+		}
+	})
 }
 
 module.exports = auth; 
