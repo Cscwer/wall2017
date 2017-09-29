f01d85c533aa01ba8ece3ed93df5dcd33648d5e1
eczn <1975845422@qq.com>
2017-09-30 01:58:27 +0800

    用户的模糊查找

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,27 @@
 const express = require('express')
     , router = express.Router()
     , rps = require('../utils/rps')
+    , { userModel } = require('../utils/db')
 
 router.get('/me', function(req, res, next){
 	rps.send2000(res, req.user); 
 });
+
+router.get('/', function(req, res){
+	let { q } = req.query; 
+
+	let EXP = new RegExp(q); 
+
+	userModel.find({
+		$or: [{
+			phone: EXP
+		}, {
+			nickname: EXP
+		}]
+	}).then(docs => {
+		rps.send2000(res, docs); 
+	})
+})
 
 module.exports = router; 
 
