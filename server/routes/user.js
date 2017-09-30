5efaa042b827ff68dee64ce2d9a5b069c60f5fc1
eczn <1975845422@qq.com>
2017-09-30 14:31:35 +0800

    update 接口

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,12 +3,14 @@ const express = require('express')
     , router = express.Router()
     , rps = require('../utils/rps')
     , { userModel } = require('../utils/db')
+    , auth = require('../utils/auth')
 
 router.get('/me', function(req, res, next){
 	rps.send2000(res, req.user); 
 });
 
 router.get('/', function(req, res){
+	let N = 10; 
 	let { q } = req.query; 
 
 	let EXP = new RegExp(q); 
@@ -19,10 +21,31 @@ router.get('/', function(req, res){
 		}, {
 			nickname: EXP
 		}]
-	}).then(docs => {
+	}).limit(N).then(docs => {
 		rps.send2000(res, docs); 
 	})
-})
+});
+
+router.post('/update', function(req, res){
+	let user_id = req.user._id; 
+	let token = req.cookies['user-token']; 
+
+	let body = req.body; 
+
+	userModel.updateOneAndCache({
+		_id: user_id
+	}, req.body).then(newUser => {
+		let newToken = auth.en(newUser); 
+
+		res.cookie('user-token', newToken, {
+			expires: new Date('2017-11-11')
+		});
+
+		rps.send2000(res, newUser); 
+	}).catch(err => {
+		console.log(err); 
+		rps.send5099(res, err); 
+	});
+}); 
 
 module.exports = router; 
-
