5e91d8d24d36ae09e3f105dc958beecbbf100501
eczn <1975845422@qq.com>
2017-10-02 22:03:31 +0800

    暴露了些接口

---
+++ server/routes/wx.js
@@ -0,0 +1,25 @@
+// wx.js
+const express = require('express')
+    , router = express.Router()
+    , rps = require('../utils/rps')
+    , vwx = require('../utils/vwx')
+
+router.get('/config', function(req, res){
+	let url = decodeURIComponent(req.query.url); 
+
+	vwx.sdk.wxConfig(url).then(result => {
+		console.log(url)
+		rps.send2000(res, result); 	
+	}); 
+}); 
+
+router.get('/img', function(req, res){
+	let { serverId } = req.query; 
+
+
+	vwx.media2url(serverId).then(bodyObj => {
+		rps.send2000(res, bodyObj); 
+	}); 
+});
+
+module.exports = router; 
