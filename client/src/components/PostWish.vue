6dfb55e17ccb68b5bcf2bf7133e67dcb86d9ecd2
eczn <1975845422@qq.com>
2017-10-20 22:47:03 +0800

    [A] 发布

---
+++ client/src/components/PostWish.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="post-wish">
+	</div>
+</template>
+
+<script>
+import wait from '@/utils/wait'; 
+import http from '@/utils/http.client'; 
+
+export default {
+	name: 'post-wish',
+	data() {
+		return {
+			
+		}
+	},
+	created(){
+
+	},
+	methods: {
+
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
