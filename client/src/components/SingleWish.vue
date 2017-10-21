8c39a785f3c4e32c25c2b5580799f0df217d9800
ruiwenruiwen <493143643@qq.com>
2017-10-21 16:15:26 +0800

    little

---
+++ client/src/components/SingleWish.vue
@@ -0,0 +1,26 @@
+<!-- SingleWish.vue -->
+
+<template>
+	<span>wwish</span>
+	<!-- <div class="wish-container">
+		<div class="user-info">
+			<img :src="user.headimgurl" class="avatar" />
+			<span class="user-name">{{userName}}</span>
+			<div class="area">{{area}}</div>
+		</div>
+	</div> -->
+</template>
+
+<script>
+import wait from '@/utils/wait';
+import http from '@/utils/http.client';
+
+export default {
+	name: 'SingleWish'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- client/src/components/Wall.vue
+++ client/src/components/Wall.vue
@@ -1,10 +1,10 @@
 <template>
 	<div v-infinite-scroll="loadMore"
-		infinite-scroll-disabled="loading" 
+		infinite-scroll-disabled="loading"
 		infinite-scroll-distance="20"
-		
+
 		class="hello">
-		<img :src="user.headimgurl" class="avatar" />
+		<!-- <img :src="user.headimgurl" class="avatar" />
 		<p class="name">{{ user.nickname }} {{ user.sex }}</p>
 
 		<div class="test">
@@ -12,7 +12,7 @@
 		</div>
 
 		<button @click="postWish">发送</button>
-		
+
 
 		<h1>弹出层</h1>
 		<button class="present" @click="present('alert')">alert</button>
@@ -29,24 +29,23 @@
 			</li>
 		</ul>
 
-		<footer>Night's Watch</footer>
+		<footer>Night's Watch</footer> -->
 	</div>
 </template>
 
 <script>
-import wait from '@/utils/wait'; 
-import http from '@/utils/http.client'; 
- 
+import wait from '@/utils/wait';
+import http from '@/utils/http.client';
 
 export default {
 	name: 'hello',
 	data() {
 		return {
-			user: {}, 
+			user: {},
 			wish: {
 				text: ''
 			},
-			list: [], 
+			list: [],
 			p: 0,
 			loading: false,
 			finish: false,
@@ -55,91 +54,91 @@ export default {
 		}
 	},
 	created(){
-		// Call Async Function 
-		this.initAll(); 
-		// this.loadMore(); 
+		// Call Async Function
+		// this.initAll();
+		// this.loadMore();
 
-		
+
 		// this.present('prompt')
 	},
 	methods: {
-		present(type){
-			this.$popup.push({
-				type: type, 
-				confirmText: '确定', 
-				cancelText: '否定',
-				placeholderText: '输入给她的留言', 
-				needBlur: true, 
-				handle: {
-					confirm(e){
-						console.log('yes', this);
-						console.log('参数', e); 
-					},
-					cancel(){
-						console.log('no')
-						this.close(); 
-					}
-				}
-			}).launch()
-		},
-		itit: function(){
-			console.log('确认'); 
-		},
-		initAll: async function(){
-			let res = await http.get('/api/user/me'); 
+		// present(type){
+		// 	this.$popup.push({
+		// 		type: type,
+		// 		confirmText: '确定',
+		// 		cancelText: '否定',
+		// 		placeholderText: '输入给她的留言',
+		// 		needBlur: true,
+		// 		handle: {
+		// 			confirm(e){
+		// 				console.log('yes', this);
+		// 				console.log('参数', e);
+		// 			},
+		// 			cancel(){
+		// 				console.log('no')
+		// 				this.close();
+		// 			}
+		// 		}
+		// 	}).launch()
+		// },
+		// itit: function(){
+		// 	console.log('确认');
+		// },
+		// initAll: async function(){
+		// 	let res = await http.get('/api/user/me');
 
-			this.user = res.data;
-		},
-		getUser: function(){
-			return http.get('/api/user/me');
-		},
-		postWish: function(){
-			let o = JSON.parse(JSON.stringify(this.wish)); 
-			
-			http.post('/api/wish', o); 
-		}, 
-		loadMore: async function(){
-			let p = this.p; 
+		// 	this.user = res.data;
+		// },
+		// getUser: function(){
+		// 	return http.get('/api/user/me');
+		// },
+		// postWish: function(){
+		// 	let o = JSON.parse(JSON.stringify(this.wish));
 
-			this.loading = true; 
+		// 	http.post('/api/wish', o);
+		// },
+		// loadMore: async function(){
+		// 	let p = this.p;
 
-			let rps = await http.get('/api/wish', {
-				p: p
-			}); 
+		// 	this.loading = true;
 
-			this.p = p + 1; 
+		// 	let rps = await http.get('/api/wish', {
+		// 		p: p
+		// 	});
 
-			this.list = this.list.concat(rps.data);
+		// 	this.p = p + 1;
 
-			if (rps.code === 2001){
-				this.finish = true; 
-			} else {
-				// this.loading = false; 
-			}
-		}
+		// 	this.list = this.list.concat(rps.data);
+
+		// 	if (rps.code === 2001){
+		// 		this.finish = true;
+		// 	} else {
+		// 		// this.loading = false;
+		// 	}
+		// }
 	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.avatar {
-	width: 10em; 
-	height: 10em; 
-	border-radius: 10em; 
+/*.avatar {
+	width: 10em;
+	height: 10em;
+	border-radius: 10em;
 }
 
 h1 {
-	font-size: 130%; 
+	font-size: 130%;
 }
 
 .present {
-	padding: .5em; 
-	margin: .5em; 
+	padding: .5em;
+	margin: .5em;
 }
 
 .name {
-	margin: 1em; 
-	font-size: 140%; 
-}
+	margin: 1em;
+	font-size: 140%;
+}*/
 </style>
