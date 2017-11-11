d5f77201e7292283b0a42d972e2e3a8e9f2da8b8
eczn <1975845422@qq.com>
2017-11-11 22:51:36 +0800

    [A] 添加了愿望动态的列表、还需要做 inner

---
+++ client/src/components/WishMsgList.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="wish-msg-list">
+		hello world
+	</div>
+</template>
+
+<script>
+// import ui from '@/utils/ui'; 
+import http from '@/utils/http.client';
+import chat from '@/utils/chat'; 
+
+export default {
+	name: 'wish-msg-list', 
+	data(){
+		return {
+			
+		}
+	},
+	created(){
+		console.log('on load')
+	}
+}
+</script>
+
+<style>
+
+
+</style>
