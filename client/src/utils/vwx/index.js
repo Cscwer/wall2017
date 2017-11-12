89e906d9b82b9136190900571b9cc12a684423d0
ruiwenruiwen <493143643@qq.com>
2017-11-12 17:31:29 +0800

    告诉他

--- client/src/utils/vwx/index.js
+++ client/src/utils/vwx/index.js
@@ -1,74 +1,74 @@
 // vwx.js
-import _ from '@/utils/vwx/wx.promise'; 
-import http from '@/utils/http.client'; 
+import _ from '@/utils/vwx/wx.promise';
+import http from '@/utils/http.client';
 
 let jsApiList = [
-	'chooseImage', 
-	'previewImage', 
-	'uploadImage', 
+	'chooseImage',
+	'previewImage',
+	'uploadImage',
 	'downloadImage',
 	'previewImage'
 ];
-let DEBUG_MODE = false; 
-let vwx = {}; 
-let URL = null; 
+let DEBUG_MODE = false;
+let vwx = {};
+let URL = null;
 
 function get_url(){
-	let { href, hash } = location; 
-	let url = href.replace(hash, ''); 
+	let { href, hash } = location;
+	let url = href.replace(hash, '');
 
-	return url; 
+	return url;
 }
 
 function check(){
-	let nowURL = get_url(); 
+	let nowURL = get_url();
 
 	if (nowURL === URL){
-		return true; 
+		return true;
 	} else {
-		return false; 
+		return false;
 	}
 }
 
 vwx.config = cb => {
-	let config_url = get_url(); 
+	let config_url = get_url();
 	return http.get('/api/wx/config', {
 		url: encodeURIComponent( config_url )
 	}).then(res => {
-		let info = res.data; 
-		info.debug = DEBUG_MODE; 
-		info.jsApiList = jsApiList; 
+		let info = res.data;
+		info.debug = DEBUG_MODE;
+		info.jsApiList = jsApiList;
 
-		wx.config(info); 
+		wx.config(info);
 
 		return new Promise((res, rej) => {
 			wx.ready(cb || function(){
-				URL = config_url; 
+				URL = config_url;
 
-				res(config_url); 
-			}); 
+				res(config_url);
+			});
 			wx.error(err => {
-				console.log('CONFIG ERR', err); 
-				rej(err); 
-			}); 
+				console.log('CONFIG ERR', err);
+				rej(err);
+			});
 		})
 	})
 }
 
 vwx.create = function(name, fn){
 	this[name] = () => {
-		let arr = Array.prototype.slice.call(arguments); 
+		let arr = Array.prototype.slice.call(arguments);
 
 		if (check()){
-			console.log(`[CONFIG: OK] Calling vwx.${name}`); 
+			console.log(`[CONFIG: OK] Calling vwx.${name}`);
 			return fn.apply(vwx, arr);
 		} else {
-			console.log(`[CONFIG: FAIL] ReConfig ... `); 
+			console.log(`[CONFIG: FAIL] ReConfig ... `);
 
 			return vwx.config().then(ok => {
-				console.log(`[CONFIG: OK] Calling vwx.${name}`); 
+				console.log(`[CONFIG: OK] Calling vwx.${name}`);
 
-				return fn.apply(vwx, arr); 
+				return fn.apply(vwx, arr);
 			})
 		}
 	}
@@ -80,34 +80,53 @@ vwx.create('chooseImage', function(){
 	    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
 	    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
 	});
-}); 
+});
 
 vwx.create('getAnImg', function(){
 	return vwx.chooseImage().then(res => {
-		let localId = res.localIds[0]; 
-		
+		let localId = res.localIds[0];
+
 		return _.uploadImage({
-			localId: localId, 
+			localId: localId,
 			isShowProgressTips: 1
 		})
 	}).then(res => {
-		let serverId = res.serverId; 
+		let serverId = res.serverId;
 
 		return http.get('/api/wx/img', {
 			serverId: serverId
 		});
 	}).catch(err => {
-		console.error('[ ERROR ]', err); 
+		console.error('[ ERROR ]', err);
 
-		return Promise.reject(err); 
+		return Promise.reject(err);
 	});
-}); 
+});
+
+vwx.create('share', function() {
+	wx.onMenuShareAppMessage({
+        title: '快来参加椰子姑娘抽奖', // 分享标题
+        desc: '每天上亿个赠饮机会，等着你来拿！', // 分享描述
+        link: 'http://gw.chenpt.cc/api/entry', // 分享链接
+        imgUrl: '', // 分享图标
+        type: '', // 分享类型,music、video或link，不填默认为link
+        dataUrl: '' // 如果type是music或video，则要提供数据链接，默认为空
+    });
+    wx.onMenuShareTimeline({
+        title: '喜庆开业 豪礼巨献', // 分享标题
+        desc: '喜庆开业 豪礼巨献闺蜜3人行 抽大奖', // 分享描述
+        link: 'http://gw.chenpt.cc/api/entry', // 分享链接
+        imgUrl: '', // 分享图标
+        type: '', // 分享类型,music、video或link，不填默认为link
+        dataUrl: '' // 如果type是music或video，则要提供数据链接，默认为空
+    });
+})
 
 // vwx.create('previewImage', function(o){
-// 	return _.previewImage(o); 
+// 	return _.previewImage(o);
 // })
-vwx.previewImage = wx.previewImage; 
+vwx.previewImage = wx.previewImage;
 
-// window.vwx = vwx; 
+// window.vwx = vwx;
 
-export default vwx; 
+export default vwx;
