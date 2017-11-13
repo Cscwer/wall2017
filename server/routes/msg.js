1ab561b255e5e87bb7a892101c84a0b86413c3d7
eczn <1975845422@qq.com>
2017-11-13 22:22:49 +0800

    [F] 404的问题

--- server/routes/msg.js
+++ server/routes/msg.js
@@ -6,30 +6,30 @@ const express = require('express')
     , auth = require('../utils/auth')
 
 // 查看留言
-// router.get('/', function(req, res){
-// 	msgModel.find(req.query).then(docs => {
-// 		rps.send2000(res, docs); 
-// 	}).catch(err => {
-// 		rps.send5000(res, err); 
-// 	}); 
-// });
+router.get('/', function(req, res){
+	msgModel.find(req.query).then(docs => {
+		rps.send2000(res, docs); 
+	}).catch(err => {
+		rps.send5000(res, err); 
+	}); 
+});
 
-// // 留言
-// router.post('/', function(req, res){
-// 	let input = {
-// 		from: req.user._id, 
-// 		to: req.body.to
-// 		content: req.body.content
-// 	}
+// 留言
+router.post('/', function(req, res){
+	let input = {
+		from: req.user._id, 
+		to: req.body.to,
+		content: req.body.content
+	}
 
-// 	let data = new msgModel(input); 
+	let data = new msgModel(input); 
 
-// 	data.save().then(ok => {
-// 		rps.send2000(res, ok); 
-// 	}).catch(err => {
-// 		rps.send5000(res, err); 
-// 	})
-// });
+	data.save().then(ok => {
+		rps.send2000(res, ok); 
+	}).catch(err => {
+		rps.send5000(res, err); 
+	})
+});
 
 // // 回复别人的留言
 // router.post('/inner', function(req, res){
