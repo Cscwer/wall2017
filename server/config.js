70d0642e103e48fb0352910b87919168b11d45c8
eczn <1975845422@qq.com>
2017-11-15 20:16:25 +0800

    [M] 换成 `守夜人工作室的日常`

--- server/config.js
+++ server/config.js
@@ -1,12 +1,27 @@
 // config.js
+const os = require('os')
+    , os_type = os.type().toLowerCase(); 
+
+let r_url; 
+
+if (os_type.startsWith('windows')){
+	r_url = 'http://gw.chenpt.cc/api/entry/code'; 
+} else {
+	r_url = 'https://gw.chenpt.cc/api/entry/code'; 
+}
+
+
 module.exports = {
 	redirect_param: {
-		appid: 'wxca79087c2e5cf375', 
-		redirect_uri: 'https://gw.chenpt.cc', 
+		appid: 'wxa70c4891aeab4ae1', 
+		// redirect_uri: 'https://gw.chenpt.cc', 
+		redirect_uri: r_url, 
 		response_type: 'code', 
 		scope: 'snsapi_userinfo',
 		state: '0' 
 	},
-	AppID: 'wxca79087c2e5cf375', 
-	AppSecret: '0dfb3847153ade322b36d42323e91dba'
+	// AppID: 'wxca79087c2e5cf375', 
+	// AppSecret: '0dfb3847153ade322b36d42323e91dba'
+	AppID: 'wxa70c4891aeab4ae1',
+	AppSecret: 'ffff67117338e06faf403581ebc32830'
 }
