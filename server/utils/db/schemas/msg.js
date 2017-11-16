5ed954a612223c48c81df020807b46acc5c69feb
eczn <1975845422@qq.com>
2017-11-16 21:55:04 +0800

    [M] 完善接口

--- server/utils/db/schemas/msg.js
+++ server/utils/db/schemas/msg.js
@@ -1,6 +1,32 @@
 // msg.js
 const mongoose = require('../connect'); 
 
+let innerSchema = mongoose.Schema({
+	from: {
+		type: mongoose.Schema.Types.ObjectId, 
+		ref: 'user',
+		required: true
+	},
+	headimgurl: {
+		type: String
+	},
+	area: {
+		type: Number, 
+		default: 0
+	},
+	nickname: {
+		type: String,
+		default: '匿名用户'
+	},
+	content: {
+		type: String, 
+		required: true
+	},
+	created_at: {
+		type: Date, 
+		default: Date.now
+	}
+})
 
 let msgSchema = mongoose.Schema({
 	from: {
@@ -13,6 +39,7 @@ let msgSchema = mongoose.Schema({
 		ref: 'user', 
 		required: true
 	}, 
+	inner: [innerSchema],
 	content: {
 		type: String, 
 		min: 3, 
