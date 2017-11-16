eff71832e7bf009cdca47f54b1a46717b5d0f2ae
ruiwenruiwen <493143643@qq.com>
2017-11-16 21:01:40 +0800

    暗恋匹配 搜索问题

--- client/src/utils/http.client.js
+++ client/src/utils/http.client.js
@@ -1,12 +1,12 @@
 // http.client.js
-import axios from 'axios'; 
+import axios from 'axios';
 import logger from './logger';
 
-let http = {}; 
+let http = {};
 
 function merge(a, b){
-	var key; 
-	
+	var key;
+
 	for (key in a){
 		b[key] = a[key];
 	}
@@ -15,56 +15,58 @@ function merge(a, b){
 }
 
 function parseToSend(method, query = {}, data = {}){
-	var toSend = data; 
+	var toSend = data;
 
 	if (method === 'get'){
-		toSend.params = query; 
+		toSend.params = query;
 		return toSend;
 	} else {
-		return merge(query, toSend); 
+		return merge(query, toSend);
 	}
 }
 
 http.send = method => function(path, query = {}, data = {}, cb){
 	if (typeof query === 'function'){
-		cb = query; 
-		query = {}; 
-	} 
+		cb = query;
+		query = {};
+	}
 
 	if (typeof data === 'function'){
 		cb = data;
-		data = query; 
+		data = query;
 	}
 
-	let toSend = parseToSend(method, query, data); 
+	let toSend = parseToSend(method, query, data);
 
 	logger.req(method, path, toSend);
 
 	return axios[method](path, toSend).then(httpRes => {
-		logger.res(method, path, httpRes); 
+		logger.res(method, path, httpRes);
 
-		cb && cb(httpRes.data); 
+		cb && cb(httpRes.data);
 
-		return httpRes.data; 
+		return httpRes.data;
 	}, err => {
-		console.log(err); 
+		console.log(err);
+
+		return Promise.reject(err);
 	})
 }
 
-http.get = http.send('get'); 
-http.post = http.send('post'); 
+http.get = http.send('get');
+http.post = http.send('post');
 
-var constFunc = a => a; 
+var constFunc = a => a;
 
 var queryStringify = o => {
 	return Object.keys(o).map(key => {
-		// let val = encodeURIComponent(o[key]); 
-		let val = o[key]; 
+		// let val = encodeURIComponent(o[key]);
+		let val = o[key];
 		return [ key, val ]
 	}).map(temp => {
-		let [key, val] = temp; 
+		let [key, val] = temp;
 
-		return `${key}=${val}`; 
+		return `${key}=${val}`;
 	}).join('&')
 }
 
@@ -77,14 +79,14 @@ function getFuncName(){
 }
 
 http.jsonp = function(url, funcName){
-	console.log('FULLURL', url); 
+	console.log('FULLURL', url);
 
-	logger.req('JSONP', url, {}); 
+	logger.req('JSONP', url, {});
 
 	return new Promise((res, rej) => {
 		// 设置不重复的回调
 		window[funcName] = httpRaw => {
-			res(httpRaw); 
+			res(httpRaw);
 		}
 
 		// DOM 操作
@@ -94,60 +96,60 @@ http.jsonp = function(url, funcName){
 		script.setAttribute('src', url);
 		body.appendChild(script);
 	}).then(JSON.parse).then(httpResponse => {
-		logger.res('JSONP', url, httpResponse); 
-		return httpResponse; 
-	}); 
+		logger.res('JSONP', url, httpResponse);
+		return httpResponse;
+	});
 }
 
 http.mixer = function(url, query){
-	return url + '&' + queryStringify(query); 
+	return url + '&' + queryStringify(query);
 }
 
 http.proxy = function(fullurl){
-	let proxyer = 'https://bird.ioliu.cn/v2'; 
-	let funcName = getFuncName(); 
+	let proxyer = 'https://bird.ioliu.cn/v2';
+	let funcName = getFuncName();
 	return {
-		fullurl: proxyer + '?url=' + fullurl + '&callback=' + funcName, 
+		fullurl: proxyer + '?url=' + fullurl + '&callback=' + funcName,
 		funcName: funcName
-	} 
+	}
 }
 
 http.bird = (kugouURL, queryInit) => (query = {}) => {
-	// Set Default Value 
-	query = queryInit(query) || {}; 
+	// Set Default Value
+	query = queryInit(query) || {};
 
 	let { fullurl, funcName } = http.proxy(
 		http.mixer(kugouURL, query)
-	); 
+	);
 
 	// send jsonp request
-	return http.jsonp(fullurl, funcName); 
+	return http.jsonp(fullurl, funcName);
 }
 
 http.musicSearch = http.bird(
-	'http://mobilecdn.kugou.com/api/v3/search/song', 
+	'http://mobilecdn.kugou.com/api/v3/search/song',
 	query => {
-		query.page      =  query.page     ||  0; 
-		query.pagesize  =  query.pagesize || 20; 
-		query.format    =  'json'; 
-		return query; 
+		query.page      =  query.page     ||  0;
+		query.pagesize  =  query.pagesize || 20;
+		query.format    =  'json';
+		return query;
 	}
-); 
+);
 
 http.musicDetail = http.bird(
-	'http://m.kugou.com/app/i/getSongInfo.php', 
+	'http://m.kugou.com/app/i/getSongInfo.php',
 	query => {
-		query.cmd = 'playInfo'; 
-		return query; 
+		query.cmd = 'playInfo';
+		return query;
 	}
-); 
+);
 
 // http.musicDetail = function(query){
-// 	let kugouURL =  
-	
+// 	let kugouURL =
+
 // 	let { fullurl, funcName } = http.proxy(
 // 		this.mixer(kugouURL, query)
-// 	); 
+// 	);
 // }
 
-export default http; 
+export default http;
